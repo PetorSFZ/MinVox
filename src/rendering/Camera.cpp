2fcf3a1dc23bde52c2ab3d6b364fd0b48ea47b72
Peter Hillerström <peter.hillerstrom93@gmail.com>
2015-02-21 21:40:20 +0100

    Removed some anoying Visual Studio warnings and fixed an error.

--- src/rendering/Camera.cpp
+++ src/rendering/Camera.cpp
@@ -7,8 +7,8 @@ namespace vox {
 
 Camera::Camera()
 {
-	mPos = sfz::vec3f{-3, 1.2f, 0.2};
-	mDir = sfz::vec3f{1, -0.1f, 0}.normalize();
+	mPos = sfz::vec3f{-3.0f, 1.2f, 0.2f};
+	mDir = sfz::vec3f{1.0f, -0.1f, 0.0f}.normalize();
 	mUp = sfz::vec3f{0, 1, 0};
 	mFov = 75;
 	mViewMatrix = sfz::lookAt(mPos, mPos + mDir, mUp);
