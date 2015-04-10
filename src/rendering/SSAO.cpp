37e7b505ae2cb40f5f1b2814a6fa21f861e25a7b
Peter Hillerström <peter.hillerstrom93@gmail.com>
2015-04-10 12:22:17 +0200

    SSAO: Reproduced previous broken result with kernel generated on CPU and then uploaded to GPU.

--- src/rendering/SSAO.cpp
+++ src/rendering/SSAO.cpp
@@ -64,28 +64,10 @@ GLuint compileSSAOShaderProgram()
 			float linearDepth = linearizeDepth(depth);
 			vec3 vsPos = texture(positionTexture, textureCoord).rgb;
 
-			/*int kernelSize = 16;
-			vec3 kernel[16] = vec3[]( vec3(0.53812504, 0.18565957, -0.43192),
-			                          vec3(0.13790712, 0.24864247, 0.44301823),
-			                          vec3(0.33715037, 0.56794053, -0.005789503),
-			                          vec3(-0.6999805, -0.04511441, -0.0019965635),
-			                          vec3(0.06896307, -0.15983082, -0.85477847),
-			                          vec3(0.056099437, 0.006954967, -0.1843352),
-			                          vec3(-0.014653638, 0.14027752, 0.0762037),
-			                          vec3(0.010019933, -0.1924225, -0.034443386),
-			                          vec3(-0.35775623, -0.5301969, -0.43581226),
-			                          vec3(-0.3169221, 0.106360726, 0.015860917),
-			                          vec3(0.010350345, -0.58698344, 0.0046293875),
-			                          vec3(-0.08972908, -0.49408212, 0.3287904),
-			                          vec3(0.7119986, -0.0154690035, -0.09183723),
-			                          vec3(-0.053382345, 0.059675813, -0.5411899),
-			                          vec3(0.035267662, -0.063188605, 0.54602677),
-			                          vec3(-0.47761092, 0.2847911, -0.0271716) );*/
-
-			float occlusion = 0.0f;
+			float occlusion = 0.0;
 
 			for (int i = 0; i < kernelSize; i++) {
-				vec3 samplePos = vsPos + kernel[i] * 10.0;
+				vec3 samplePos = vsPos + kernel[i] * 0.1;
 				
 				vec4 sampleOffset = vec4(samplePos, 1.0);
 				sampleOffset = projectionMatrix * sampleOffset;
@@ -95,7 +77,7 @@ GLuint compileSSAOShaderProgram()
 				float sampleDepth = texture(depthTexture, sampleOffset.xy).r;
 				float sampleLinearDepth = linearizeDepth(sampleDepth);
 
-				occlusion += (sampleLinearDepth <= samplePos.z ? 1.0 : 0.0) * (1.0/16.0) * 2.0;
+				occlusion += (sampleLinearDepth <= samplePos.z ? 1.0 : 0.0) * (1.0/16.0);
 			}
 
 			if (textureCoord.x > 600) fragmentColor = vec4(vec3(occlusion), 1.0);
