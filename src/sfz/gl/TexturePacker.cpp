38195834978750c05db15bffd5cadfc83657f079
Peter Hillerström <peter@hstroem.se>
2015-05-12 23:24:07 +0200

    TexturePacker: More work.

--- src/sfz/gl/TexturePacker.cpp
+++ src/sfz/gl/TexturePacker.cpp
@@ -1,11 +1,113 @@
 #include "sfz/gl/TexturePacker.hpp"
 
+#include <SDL.h>
+#include "sfz/gl/Utils.hpp"
+
+//#define STB_RECT_PACK_IMPLEMENTATION
+#include "sfz/gl/font/stb_rect_pack.h"
+
 #include <sfz/MSVC12HackON.hpp>
 
 namespace sfz {
 
+// Anonymous namespace
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
 
+namespace {
 
+// Simple function that flips a surface by byte-level manipulation.
+void flipSurface(SDL_Surface* surface) noexcept
+{
+	// Locking the surface
+	if (SDL_LockSurface(surface) < 0) {
+		std::cerr << "Couldn't lock surface for image flipping: " << SDL_GetError() << std::endl;
+	}
+
+	// Constants
+	const int w = surface->w;
+	const int h = surface->h;
+	const int pixelCount = w * h;
+	const int bytesPerPixel = surface->format->BytesPerPixel;
+	const int bytesPerRow = w * bytesPerPixel;
+	const int totalByteCount = pixelCount * bytesPerPixel;
+	Uint32* const surfacePixels = static_cast<Uint32*>(surface->pixels);
+	Uint32* const pixelBuffer = new Uint32[pixelCount];
+
+	// surfPtr reads from surface, starting value is first pixel.
+	Uint32* surfPtr = surfacePixels;
+
+	// bufPtr writes to buffer, starting value is the first pixel outside the image
+	Uint32* bufPtr = pixelBuffer + pixelCount;
+
+	// Copy pixels from surface to buffer until all pixels are copied
+	while (bufPtr > pixelBuffer) {
+		bufPtr = bufPtr - w; // Move bufPtr back one image row
+		std::memcpy(bufPtr, surfPtr, bytesPerRow); // Copy one image row to buffer
+		surfPtr = surfPtr + w; // Move surfPtr forward one image row
+	}
+
+	// Copy pixels back from buffer and free memory.
+	std::memcpy(surfacePixels, pixelBuffer, totalByteCount);
+	delete[] pixelBuffer;
+
+	// Unlocking the surface
+	SDL_UnlockSurface(surface);
+}
+
+SDL_Surface* loadTexture(const std::string& path) noexcept
+{
+	// Load specified surface.
+	SDL_Surface* surface = NULL;
+	surface = IMG_Load(path.c_str());
+	if (surface == NULL) {
+		std::cerr << "Unable to load image at: " << path << ", error: " << IMG_GetError();
+		std::terminate();
+	}
+
+	// Some error checking.
+	if (surface->format->BytesPerPixel != 4) {
+		std::cerr << "Image at \"" << path << "\" doesn't have 4 bytes per pixel." << std::endl;
+		std::terminate();
+	}
+	if (!SDL_ISPIXELFORMAT_ALPHA(surface->format->format)) {
+		std::cerr << "Image at \"" << path << "\" doesn't contain alpha channel." << std::endl;
+		std::terminate();
+	}
+
+	// Flips surface so UV coordinates will be in a right-handed system in OpenGL.
+	flipSurface(surface);
+
+	return surface;
+}
+
+} // anonymous namespace
+
+// TexturePacker: Constructors & destructors
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+
+TexturePacker::TexturePacker(const vector<string>& paths) noexcept
+:
+	mPaths{paths},
+	mTexRegions{paths.size()}
+{
+	vector<SDL_Surface*> surfaces{paths.size()};
+	vector<struct stbrp_rect> rects{paths.size()};
+	for (auto& path : paths) {
+		surfaces.emplace_back(loadTexture(path));
+		struct stbrp_rect r;
+		r.id = surfaces.size()-1;
+		r.w = surfaces.back()->w;
+		r.h = surfaces.back()->h;
+		rects.push_back(r);
+	}
+
+	glGenTextures(1, &mTexture);
+}
+
+TexturePacker::~TexturePacker() noexcept
+{
+	glDeleteTextures(1, &mTexture);
+}
 
 } // namespace sfz
 
--- src/sfz/gl/TexturePacker.hpp
+++ src/sfz/gl/TexturePacker.hpp
@@ -2,9 +2,21 @@
 #ifndef SFZ_GL_TEXTURE_PACKER_HPP
 #define SFZ_GL_TEXTURE_PACKER_HPP
 
+#include <sfz/gl/OpenGL.hpp>
+#include <sfz/gl/Texture.hpp>
+#include <sfz/gl/TextureRegion.hpp>
+
+#include <cstddef> // size_t
+#include <vector>
+#include <string>
+
 #include <sfz/MSVC12HackON.hpp>
 
 namespace sfz {
+
+using std::size_t;
+using std::vector;
+using std::string;
 
 // TexturePacker
 // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
@@ -20,11 +32,22 @@ public:
 	TexturePacker& operator= (const TexturePacker&) = delete;
 	TexturePacker& operator= (TexturePacker&&) = delete;
 
-	TexturePacker(const char** str) noexcept;
+	TexturePacker(const vector<string>& paths) noexcept;
+	~TexturePacker() noexcept;
+
+	// Public methods
+	// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+
+	inline GLuint texture() const noexcept { return mTexture; }
 
 private:
+	// Private members
+	// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
 
-
+	vector<string> mPaths;
+	GLuint mTexture;
+	size_t mTexWidth, mTexHeight;
+	vector<TextureRegion> mTexRegions;
 };
 
 } // namespace sfz
