9072d399404272e35c6b081ddc39804af1a55f85
Peter Hillerström <peter@hstroem.se>
2015-07-12 23:02:03 +0200

    Squashed 'externals/SkipIfZeroGraphics/' changes from 6d5b9aa..10c1df3
    10c1df3 Texture: RGBA by default.
    7885cee WIP: More work on SDL_image -> stb_image refactor.
    b1e48cf WIP: New flip image function in Texture.
    11aa603 WIP: Removed SDL_image dependency and replaced with bundled stb_image. Still untested and a few bugs will surely pop up before complete.
    
    git-subtree-dir: externals/SkipIfZeroGraphics
    git-subtree-split: 10c1df37433771877bd89edbf78d3e3e7d5eca63

--- include/sfz/gl/Texture.hpp
+++ include/sfz/gl/Texture.hpp
@@ -3,12 +3,21 @@
 #define SFZ_GL_TEXTURE_HPP
 
 #include <string>
+#include <cstdint>
 #include "sfz/gl/OpenGL.hpp"
 #include "sfz/gl/Utils.hpp"
 
 namespace gl {
 
 using std::string;
+using std::uint8_t;
+
+enum class TextureFormat : uint8_t {
+	GRAY = 1, // Gray == color.r
+	GRAY_ALPHA = 2, // Gray == color.r, Alpha == color.g
+	RGB = 3,
+	RGBA = 4
+};
 
 class Texture final {
 public:
@@ -24,12 +33,10 @@ public:
 	Texture(const Texture&) = delete;
 	Texture& operator= (const Texture&) = delete;
 	
-	Texture(const string& path) noexcept;
+	Texture(const string& path, TextureFormat format = TextureFormat::RGBA) noexcept;
 	Texture& operator= (Texture&& other) noexcept;
 	~Texture() noexcept;
 };
 
 } // namespace gl
-
-
 #endif
